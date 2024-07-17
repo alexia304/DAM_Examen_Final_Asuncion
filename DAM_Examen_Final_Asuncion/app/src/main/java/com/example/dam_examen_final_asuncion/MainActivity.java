7ad48940838d2d3f11dd27a47a3cc56ffe047131
Alexia Asuncion <myscene_649@hotmail.com>
2024-07-17 10:16:21 -0500

    Notas en columnas

--- DAM_Examen_Final_Asuncion/app/src/main/java/com/example/dam_examen_final_asuncion/MainActivity.java
+++ DAM_Examen_Final_Asuncion/app/src/main/java/com/example/dam_examen_final_asuncion/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.dam_examen_final_asuncion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnRegistrar = findViewById(R.id.buttonRegistrar);
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NotasActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
